083eead93cfa0688491ebb0fc5a7eb85ad5d9170
unknown <99054420@mydavinci.nl>
2021-12-10 10:54:36 -0800

    update

--- tries.py
+++ tries.py
@@ -1,20 +1,32 @@
-from random import randint
-
-
 import random
 def wachtwoord():
+    length = 24
     numbers = list('1234567890')
     lowerC = list('abcdefghijklmnopqrstuvwxyz')
     upperC = list('ABDCEFGHIJKLMNOPQRSTUVWXYZ')
     symbols = list('@#$%&_')
+    num = []
+    up = []
+    low = []
+    sym = []
     password = []
-    for i in range(7):
-        password.append(random.choice(numbers))
+    for i in range(random.randint(4,7)):
+        num.append(random.choice(numbers))
     for i in range(8):
-        password.append(random.choice(lowerC))
+        low.append(random.choice(lowerC))
     for i in range(3):
-        password.append(random.choice(symbols))
-    for i in range(6):
-        password.append(random.randint(upperC))
-    print(password)
+        sym.append(random.choice(symbols))
+    for i in range(random.randint(2,6)):
+        up.append(random.choice(upperC))
+    password = num + up +low +sym
+    amount = 24-len(password)
+    if len(password)< length:
+        choice3 = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(amount)]
+        password = num + up +low +sym+choice3
+    elif  password [0:2] == num and password[0:2] ==sym  and password[0:-1] == sym :
+        wachtwoord()
+    random.shuffle(password)
+    print(''.join(password))
+
+    
 wachtwoord()
--- wachtwrood.py
+++ wachtwrood.py
@@ -1,11 +1,20 @@
 
 import random
 
-choice = [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(3)]
-choice1 = [random.choice('1234567890') for i in range(7)]
-choice2 = [random.choice('!@#$%^&*()') for i in range(3)]
-choice3 = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(8)]
-choice4 = [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890') for i in range(3)]
-choices = choice1+choice2+choice3
-random.shuffle(choices)
-print('hier is uw wachtwoord met 24 characters:',''.join(choice+choices+choice4))
+choiceLC = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(2)]
+choiceUP = [random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(random.randint(1,3))]
+choiceNUM = [random.choice('1234567890')for i in range(random.randint(4,7))]
+choiceSYM = [random.choice('!@#$%^&*()')for i in range(random.randint(3, 3))]
+
+choice_LW_UP = choiceLC+choiceUP
+random.shuffle(choice_LW_UP)
+choiceALL = choiceLC+choiceUP+choiceNUM+choiceSYM
+random.shuffle(choiceALL)
+choiceTotall = choice_LW_UP+choiceALL
+choiceSUM = 24-len(choiceTotall)
+choiceADD = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(choiceSUM)]
+if len(choiceTotall) < 24:
+    print(''.join(choiceTotall+choiceADD))
+    print('aantal characters:', len(choiceTotall+choiceADD))
+else:
+    print(choiceTotall)
